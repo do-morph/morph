d795fe932f24c3f1e877b80ee358b5a9f2066c96
Mateus Pinheiro <mateusp@br.ibm.com>
2016-07-21 16:32:17 -0300

    Create the variable import structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp          = require('gulp');
-var postcss       = require('gulp-postcss');
-var autoprefixer  = require('autoprefixer');
-var cssnext       = require('cssnext');
-var postcssImport = require('postcss-import');
+var gulp             = require('gulp');
+var postcss          = require('gulp-postcss');
+var autoprefixer     = require('autoprefixer');
+var cssnext          = require('cssnext');
+var postcssImport    = require('postcss-import');
+var customProperties = require('postcss-custom-properties');
 
 gulp.task('default', function() {
 	gulp.watch('./modules/**/*.css', ['css']);
@@ -12,6 +13,7 @@ gulp.task('css', function() {
 	var processors = [
 		postcssImport,
 		autoprefixer,
+		customProperties,
 		cssnext
 	];
 
